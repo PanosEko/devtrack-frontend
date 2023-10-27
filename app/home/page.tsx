b053e600857d59083e5ad6db5503b4746fea34b6
PanosEko <93736094+PanosEko@users.noreply.github.com>
2023-10-27 15:46:22 +0300

    First deploy

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,38 +9,38 @@ import {LoadingScreen} from "@/components/LoadingScreen";
 
 export default function Home() {
     const router = useRouter();
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
 
-    useEffect(() => {
-        refreshToken().then(() =>{
-            setLoading(false);
-        } )
-            .catch((error:any) =>{
-                router.push('/login');
-                setTimeout(() => {
-                    toast.error("You are not logged in!");
-                }, 1000);
-            })
-        const interval = setInterval(() => {
-            refreshToken().then(() =>{} )
-                .catch((error:any) =>{
-                    router.push('/login');
-                })
-        }, 14 * 60 * 1000);
-        // Clean up the interval when the component is unmounted
-        return () => {
-            clearInterval(interval);
-        };
-    }, [router]);
-
-if(loading){
-    return (
-        <div>
-            <LoadingScreen/>
-        </div>
-    );
-}
+//     useEffect(() => {
+//         refreshToken().then(() =>{
+//             setLoading(false);
+//         } )
+//             .catch((error:any) =>{
+//                 router.push('/login');
+//                 setTimeout(() => {
+//                     toast.error("You are not logged in!");
+//                 }, 1000);
+//             })
+//         const interval = setInterval(() => {
+//             refreshToken().then(() =>{} )
+//                 .catch((error:any) =>{
+//                     router.push('/login');
+//                 })
+//         }, 14 * 60 * 1000);
+//         // Clean up the interval when the component is unmounted
+//         return () => {
+//             clearInterval(interval);
+//         };
+//     }, [router]);
+//
+// if(loading){
+//     return (
+//         <div>
+//             <LoadingScreen/>
+//         </div>
+//     );
+// }
 
     return (
         <main>
