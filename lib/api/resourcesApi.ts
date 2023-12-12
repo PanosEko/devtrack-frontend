aa8f338ebf87678fc80ef63d4731c7ee896f2c5a
PanosEko <93736094+PanosEko@users.noreply.github.com>
2023-12-12 19:16:49 +0200

    update task modified and error logs added

--- lib/api/resourcesApi.ts
+++ lib/api/resourcesApi.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const TASK_BASE_URL = "http://localhost:8080/api/v1/resources/tasks";
-const IMAGE_BASE_URL = "http://localhost:8080/api/v1/resources/images";
+// const TASK_BASE_URL = "http://localhost:8080/api/v1/resources/tasks";
+// const IMAGE_BASE_URL = "http://localhost:8080/api/v1/resources/images";
 
-// const API_BASE_URL = 'https://api.devtrack.dedyn.io/api/v1/task';
+const TASK_BASE_URL = 'https://api.devtrack.dedyn.io/api/resources/v1/tasks';
+const IMAGE_BASE_URL = 'https://api.devtrack.dedyn.io/api/resources/v1/images';
+
 
 export const updateTaskInDB = async (task: Task, image: Thumbnail | null) => {
   try {
